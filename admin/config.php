6426afd8c3e409cac67f6f58bfa838df76684cd3
Eugene Baskov <ebaskiv@gmail.com>
2017-07-06 17:45:33 +0300

    initializ commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://logoshop.loc/admin/');
+define('HTTP_CATALOG', 'http://logoshop.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://logoshop.loc/admin/');
+define('HTTPS_CATALOG', 'http://logoshop.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/home/jbaskoff/public_html/logoshop.loc/admin/');
+define('DIR_SYSTEM', '/home/jbaskoff/public_html/logoshop.loc/system/');
+define('DIR_IMAGE', '/home/jbaskoff/public_html/logoshop.loc/image/');
+define('DIR_LANGUAGE', '/home/jbaskoff/public_html/logoshop.loc/admin/language/');
+define('DIR_TEMPLATE', '/home/jbaskoff/public_html/logoshop.loc/admin/view/template/');
+define('DIR_CONFIG', '/home/jbaskoff/public_html/logoshop.loc/system/config/');
+define('DIR_CACHE', '/home/jbaskoff/public_html/logoshop.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/jbaskoff/public_html/logoshop.loc/system/storage/download/');
+define('DIR_LOGS', '/home/jbaskoff/public_html/logoshop.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/jbaskoff/public_html/logoshop.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/home/jbaskoff/public_html/logoshop.loc/system/storage/upload/');
+define('DIR_CATALOG', '/home/jbaskoff/public_html/logoshop.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'djuice');
+define('DB_DATABASE', 'logoshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://logoshop.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://logoshop.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/home/jbaskoff/public_html/logoshop.loc/catalog/');
+define('DIR_SYSTEM', '/home/jbaskoff/public_html/logoshop.loc/system/');
+define('DIR_IMAGE', '/home/jbaskoff/public_html/logoshop.loc/image/');
+define('DIR_LANGUAGE', '/home/jbaskoff/public_html/logoshop.loc/catalog/language/');
+define('DIR_TEMPLATE', '/home/jbaskoff/public_html/logoshop.loc/catalog/view/theme/');
+define('DIR_CONFIG', '/home/jbaskoff/public_html/logoshop.loc/system/config/');
+define('DIR_CACHE', '/home/jbaskoff/public_html/logoshop.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/jbaskoff/public_html/logoshop.loc/system/storage/download/');
+define('DIR_LOGS', '/home/jbaskoff/public_html/logoshop.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/jbaskoff/public_html/logoshop.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/home/jbaskoff/public_html/logoshop.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'djuice');
+define('DB_DATABASE', 'logoshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
